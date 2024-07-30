392510cbd9c58fa4ebddadf08d2298c3b1b6d2ee
Mohammad Zeeshan <141507225+ZestyZeeshan@users.noreply.github.com>
2024-07-30 16:42:38 +0530

    Update Signup.jsx

--- Frontend/Frontend/src/components/Signup.jsx
+++ Frontend/Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("https://bookstore-api-lgie.onrender.com//user/signup", userInfo)
+      .post("https://bookstore-api-lgie.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
